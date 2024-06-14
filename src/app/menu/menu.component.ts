e8c9be8ab6a4a8f5802bcf3ba0bc51abc632b6db
sandeep514 <sandy.singh51480@gmail.com>
2024-06-14 12:15:08 +0530

    update platform ios

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -151,10 +151,10 @@ export class MenuComponent implements OnInit {
 			console.log(res.data)
 			if (localStorage.getItem('apptype') == 'USD') {
 				this.news = res.data['usd'][0].title;
-				this.newsWidth = (this.news.length * 8) + 'px';
+				this.newsWidth = (this.news.length * 4) + 'px';
 			} else {
 				this.news = res.data['inr'][0].title;
-				this.newsWidth = (this.news.length * 8) + 'px';
+				this.newsWidth = (this.news.length * 4) + 'px';
 			}
 
 		}, (err: any) => {
